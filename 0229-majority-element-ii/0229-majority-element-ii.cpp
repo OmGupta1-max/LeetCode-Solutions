d9e02f35da7dd4fb6587f8ce1c5c766cfa102f2a
Om Gupta <guptaom0003@gmail.com>
2026-07-24 23:01:32 +0530

    Time: 2 ms (52.05%), Space: 23.7 MB (67%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,35 +1,54 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int cand1 = 0, cand2 = 0; 
+
+        int candidate1 = 0, candidate2 = 0;
         int count1 = 0, count2 = 0;
+
+        // First Pass: Find Potential Candidates
         for (int num : nums) {
-            if (num == cand1) {
+
+            if (num == candidate1) {
                 count1++;
-            } else if (num == cand2) {
+            }
+            else if (num == candidate2) {
                 count2++;
-            } else if (count1 == 0) {
-                cand1 = num;
+            }
+            else if (count1 == 0) {
+                candidate1 = num;
                 count1 = 1;
-            } else if (count2 == 0) {
-                cand2 = num;
+            }
+            else if (count2 == 0) {
+                candidate2 = num;
                 count2 = 1;
-            } else {
+            }
+            else {
                 count1--;
                 count2--;
             }
         }
-        count1 = count2 = 0;
+
+        // Second Pass: Verify Candidates
+        count1 = 0;
+        count2 = 0;
+
         for (int num : nums) {
-            if (num == cand1) count1++;
-            else if (num == cand2) count2++;
+
+            if (num == candidate1)
+                count1++;
+
+            else if (num == candidate2)
+                count2++;
         }
 
-        vector<int> res;
-        int n = nums.size();
-        if (count1 > n / 3) res.push_back(cand1);
-        if (count2 > n / 3) res.push_back(cand2);
+        vector<int> ans;
 
-        return res;
+        if (count1 > nums.size() / 3)
+            ans.push_back(candidate1);
+
+        if (count2 > nums.size() / 3)
+            ans.push_back(candidate2);
+
+        return ans;
     }
 };
