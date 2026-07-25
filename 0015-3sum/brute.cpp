f3fdf84f3653d5e96f0a7ea88a2321ef5019ff6a
Om Gupta <guptaom0003@gmail.com>
2026-07-25 20:04:59 +0530

    Create brute.cpp

---
+++ 0015-3sum/brute.cpp
@@ -0,0 +1,34 @@
+// T.C = O(n^3) - Time Limit Exceed
+// S.C = O(Number of Triplets)
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        int n = nums.size();
+
+        set<vector<int>> st;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                for (int k = j + 1; k < n; k++) {
+
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+
+                        vector<int> temp = {nums[i], nums[j], nums[k]};
+
+                        sort(temp.begin(), temp.end());
+
+                        st.insert(temp);
+                    }
+                }
+            }
+        }
+
+        vector<vector<int>> ans(st.begin(), st.end());
+
+        return ans;
+    }
+};
