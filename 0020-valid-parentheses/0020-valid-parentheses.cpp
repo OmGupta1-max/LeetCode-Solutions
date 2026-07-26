234e3679452f920fffc64d4d0292392ed6fd6524
Om Gupta <guptaom0003@gmail.com>
2026-07-26 10:44:57 +0530

    Time: 311 ms (1.07%), Space: 8.5 MB (99.5%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        bool changed = true;
+
+        while (changed) {
+
+            changed = false;
+
+            size_t pos;
+
+            if ((pos = s.find("()")) != string::npos) {
+                s.erase(pos, 2);
+                changed = true;
+            }
+
+            if ((pos = s.find("[]")) != string::npos) {
+                s.erase(pos, 2);
+                changed = true;
+            }
+
+            if ((pos = s.find("{}")) != string::npos) {
+                s.erase(pos, 2);
+                changed = true;
+            }
+        }
+
+        return s.empty();
+    }
+};
