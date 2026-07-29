4f0219962665af5cb036d3ecb32da92de54cf959
Om Gupta <guptaom0003@gmail.com>
2026-07-29 11:29:15 +0530

    Add brute force method for max area calculation
    Implement brute force solution to find maximum area.

---
+++ 0011-container-with-most-water/brute.cpp
@@ -0,0 +1,28 @@
+// T.C = O(n^2)
+// S.C = O(1)
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+
+        int n = height.size();
+
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                int width = j - i;
+
+                int h = min(height[i], height[j]);
+
+                int area = width * h;
+
+                maxArea = max(maxArea, area);
+            }
+        }
+
+        return maxArea;
+    }
+};
