dfbae1c9daf12f6f4974c1e40b2215a8932a222c
Om Gupta <guptaom0003@gmail.com>
2026-08-01 09:56:40 +0530

    Add brute force method to find error numbers
    Implement brute force solution to find duplicate and missing numbers in an array.

---
+++ 0645-set-mismatch/brute.cpp
@@ -0,0 +1,32 @@
+// T.C = O(n^2)
+// S.C = O(1)
+
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+
+        int n = nums.size();
+
+        int duplicate = -1;
+        int missing = -1;
+
+        for (int num = 1; num <= n; num++) {
+
+            int count = 0;
+
+            for (int i = 0; i < n; i++) {
+
+                if (nums[i] == num)
+                    count++;
+            }
+
+            if (count == 2)
+                duplicate = num;
+
+            if (count == 0)
+                missing = num;
+        }
+
+        return {duplicate, missing};
+    }
+};
