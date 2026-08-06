5619d27e4e9b0cc707ed032f1db31e0bfbb434ae
Om Gupta <guptaom0003@gmail.com>
2026-08-06 15:56:39 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.19%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+          if (x < 2) return x;
+        
+        long long low = 1, high = x / 2, ans = 0;
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+            long long square = mid * mid;
+            
+            if (square == x) return mid;
+            else if (square < x) {
+                ans = mid;      
+                low = mid + 1;  
+            } else {
+                high = mid - 1; 
+            }
+        }
+        return ans;
+    }
+};
