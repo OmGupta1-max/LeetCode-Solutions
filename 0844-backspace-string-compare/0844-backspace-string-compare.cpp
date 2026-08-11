ae677ad3e66dac13d44a6bbb1cb9b0c51d690f5c
Om Gupta <guptaom0003@gmail.com>
2026-08-11 18:41:16 +0530

    Time: 0 ms (100%), Space: 8.3 MB (96.61%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,24 +1,56 @@
 class Solution {
 public:
-
-    string build(string s) {
-        string ans;
-        for (char ch : s) {
-
-            if (ch == '#') {
-                if (!ans.empty())
-                    ans.pop_back();
-            }
-            else {
-                ans.push_back(ch);
-            }
-        }
-
-        return ans;
-    }
     bool backspaceCompare(string s, string t) {
-        string a = build(s);
-        string b = build(t);
-        return a == b;
+        int i = s.size() - 1;
+        int j = t.size() - 1;
+        int skipS = 0;
+        int skipT = 0;
+        while (i >= 0 || j >= 0) {
+            // Find next valid character in s
+            while (i >= 0) {
+
+                if (s[i] == '#') {
+                    skipS++;
+                    i--;
+                }
+                else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                }
+                else {
+                    break;
+                }
+            }
+
+            // Find next valid character in t
+            while (j >= 0) {
+                if (t[j] == '#') {
+                    skipT++;
+                    j--;
+                }
+                else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                }
+                else {
+                    break;
+                }
+            }
+            // One string has a character, other doesn't
+            if (i >= 0 && j < 0)
+                return false;
+            if (i < 0 && j >= 0)
+                return false;
+             // Both have valid characters
+            if (i >= 0 && j >= 0) {
+
+                if (s[i] != t[j])
+                    return false;
+            }
+
+            i--;
+            j--;
+        }
+        return true;
     }
 };
