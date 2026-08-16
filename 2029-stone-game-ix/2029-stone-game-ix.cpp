49836f9e725c8766b6f04bdf21125a4b9420b333
Om Gupta <guptaom0003@gmail.com>
2026-08-16 11:49:57 +0530

    Time: 5 ms (48.73%), Space: 131.3 MB (33.5%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.cpp
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
     bool stoneGameIX(vector<int>& stones) {
-        // count[0] = stones divisible by 3
-        // count[1] = stones with remainder 1
-        // count[2] = stones with remainder 2
-        vector<int> count(3, 0);
-        for (int x : stones) {
-            count[x % 3]++;
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        for (int val : stones) {
+            if (int type = val % 3; type == 0) {
+                ++cnt0;
+            } else if (type == 1) {
+                ++cnt1;
+            } else {
+                ++cnt2;
+            }
         }
-        // Try Alice taking a remainder-1 stone first
-        if (canWin(count[0], count[1], count[2]))
-            return true;
-        // Try Alice taking a remainder-2 stone first
-        return canWin(count[0], count[2], count[1]);
-    }
-private:
-    bool canWin(int zero, int one, int two) {
-        // Alice must take a non-zero remainder first.
-        if (one == 0)
-            return false;
-        // Alice takes one remainder-1 stone.
-        one--;
-        // Then players must alternate between remainder 1 and 2.
-        int moves = 1 + min(one, two) * 2;
-        // If there is an extra remainder-1 stone,
-        // one more move can be made.
-        if (one > two) {
-            one--;
-            moves++;
+        if (cnt0 % 2 == 0) {
+            return cnt1 >= 1 && cnt2 >= 1;
         }
-        // The zero-remainder stones don't change the sum modulo 3.
-        moves += zero;
-        // Alice wins if:
-        // 1. The number of moves is odd.
-        // 2. Some non-zero-remainder stone is still left.
-        return moves % 2 == 1 && one != two;
+        return cnt1 - cnt2 > 2 || cnt2 - cnt1 > 2;
     }
 };
