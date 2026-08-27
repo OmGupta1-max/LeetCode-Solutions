0fa952125156838bc99026bb2fadf0862af4db87
Om Gupta <guptaom0003@gmail.com>
2026-08-27 14:59:41 +0530

    Time: 21 ms (19.11%), Space: 18.9 MB (20.38%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -1,32 +1,67 @@
 class Solution {
 public:
     string lexGreaterPermutation(string s, string target) {
-        vector<int> cnt(26);
-        for (int i = 0; i < s.size(); i++) {
-            cnt[s[i] - 'a']++;
-            cnt[target[i] - 'a']--;
+        vector<int> cnt(26, 0);
+        for (char c : s) {
+            cnt[c - 'a']++;
         }
 
-        // Try from right to left
-        for (int i = s.size() - 1; i >= 0; i--) {
-            int b = target[i] - 'a';
-            cnt[b]++;  // Reversal of consumption
-                       // Check if the prefix can fully match
-            if (*min_element(cnt.begin(), cnt.end()) < 0) {
-                continue;
+        string res;
+        int n = target.size();
+        for (int i = 0; i < n; i++) {
+            int targetChar = target[i] - 'a';
+
+            // Case 1: First try to place the same character as target[i] at the
+            // current position
+            if (cnt[targetChar] > 0) {
+                cnt[targetChar]--;
+                // Check if the remaining characters can form a string greater
+                // than target[i+1:]
+                if (canFormGreater(cnt, target, i + 1)) {
+                    res.push_back(target[i]);
+                    continue;
+                }
+                // Cannot form a larger string, backtrack
+                cnt[targetChar]++;
             }
-            // Find the smallest available character larger than b.
-            for (int j = b + 1; j < 26; j++) {
-                if (cnt[j]) {
+
+            // Case 2: Place a character greater than target[i] at the current
+            // position
+            for (int j = targetChar + 1; j < 26; j++) {
+                if (cnt[j] > 0) {
                     cnt[j]--;
-                    target[i] = 'a' + j;
-                    target.resize(i + 1);
-                    return target + getMinString(cnt);
+                    res.push_back('a' + j);
+                    // Fill remaining positions with the smallest
+                    // lexicographical order
+                    res += getMinString(cnt);
+                    return res;
                 }
             }
+
+            // No feasible solution found, return directly
+            return "";
         }
 
         return "";
+    }
+
+private:
+    // Check if the remaining characters can form a string greater than the
+    // suffix.
+    bool canFormGreater(const vector<int>& cnt, const string& target,
+                        int start) {
+        string maxStr = getMaxString(cnt);
+        string suffix = target.substr(start);
+        return maxStr > suffix;
+    }
+
+    // Get the maximum lexicographical string (in descending order)
+    string getMaxString(const vector<int>& cnt) {
+        string res;
+        for (int i = 25; i >= 0; i--) {
+            res.append(cnt[i], 'a' + i);
+        }
+        return res;
     }
 
     // Get the lexicographically smallest string (in ascending order)
