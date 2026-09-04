ace7d6e5ed27efb37a08da970e65a511665d3c99
Om Gupta <guptaom0003@gmail.com>
2026-09-04 20:07:53 +0530

    Add solution for counting smaller numbers
    Implemented a solution to count how many numbers are smaller than each number in the input array.

---
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/better.cpp
@@ -0,0 +1,18 @@
+// T.C = O(nlogn)
+// S.C = O(n)
+
+
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> sorted = nums;
+        sort(sorted.begin(), sorted.end());
+        vector<int> ans;
+        for (int num : nums) {
+            int count = lower_bound(sorted.begin(), sorted.end(), num)
+                        - sorted.begin();
+            ans.push_back(count);
+        }
+        return ans;
+    }
+};
