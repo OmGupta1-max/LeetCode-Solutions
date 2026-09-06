f84e0b097a32b1a65fec36783d806ac1551364d3
Om Gupta <guptaom0003@gmail.com>
2026-09-06 14:53:46 +0530

    Time: 0 ms (100%), Space: 95 MB (79.77%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,40 +1,46 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1,vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        int total = n + m;
-        int medianIndex = total / 2;
-        int i = 0;
-        int j = 0;
-        int previous = -1;
-        int current = -1;
-        for (int count = 0; count <= medianIndex; count++) {
-            previous = current;
-            if (i < n && j < m) {
-                if (nums1[i] <= nums2[j]) {
-                    current = nums1[i];
-                    i++;
+    double findMedianSortedArrays(
+        vector<int>& nums1,
+        vector<int>& nums2
+    ) {
+        // Always binary search on smaller array
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int low = 0;
+        int high = n1;
+        int leftSize = (n1 + n2 + 1) / 2;
+        while (low <= high) {
+            int cut1 = low + (high - low) / 2;
+            int cut2 = leftSize - cut1;
+            int left1 =(cut1 == 0)? INT_MIN: nums1[cut1 - 1];
+            int right1 =(cut1 == n1)? INT_MAX: nums1[cut1];
+            int left2 =(cut2 == 0)? INT_MIN: nums2[cut2 - 1];
+            int right2 =(cut2 == n2)? INT_MAX: nums2[cut2];
+            // Correct partition
+            if (left1 <= right2 && left2 <= right1) {
+                // Odd total elements
+                if ((n1 + n2) % 2 == 1) {
+                    return max(left1, left2);
                 }
-                else {
-                    current = nums2[j];
-                    j++;
-                }
+                // Even total elements
+                return (
+                    max(left1, left2) +
+                    min(right1, right2)
+                ) / 2.0;
             }
-            else if (i < n) {
-                current = nums1[i];
-                i++;
+            // Took too many elements from nums1
+            else if (left1 > right2) {
+                high = cut1 - 1;
             }
+            // Took too few elements from nums1
             else {
-                current = nums2[j];
-                j++;
+                low = cut1 + 1;
             }
         }
-        // Odd number of elements
-        if (total % 2 == 1) {
-            return current;
-        }
-        // Even number of elements
-        return (previous + current) / 2.0;
+        return 0;
     }
 };
