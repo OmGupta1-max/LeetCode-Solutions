623ad3c96e199dfb741cb4cd20ae85e9cf9bd316
Om Gupta <guptaom0003@gmail.com>
2026-09-14 19:34:39 +0530

    Time: 92 ms (16.46%), Space: 18.7 MB (38.74%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -6,10 +6,21 @@ public:
         int m = matrix[0].size();
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
 
-                if (matrix[i][j] == target)
+            int low = 0;
+            int high = m - 1;
+
+            while (low <= high) {
+
+                int mid = low + (high - low) / 2;
+
+                if (matrix[i][mid] == target)
                     return true;
+
+                if (matrix[i][mid] < target)
+                    low = mid + 1;
+                else
+                    high = mid - 1;
             }
         }
 
