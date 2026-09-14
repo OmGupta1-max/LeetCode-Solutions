4e8daf853ae53f832aeed4b86b1c69a8eaa4cba7
Om Gupta <guptaom0003@gmail.com>
2026-09-14 19:56:35 +0530

    Add searchMatrix function for 2D matrix
    Implement binary search for 2D matrix.

---
+++ 0240-search-a-2d-matrix-ii/better.cpp
@@ -0,0 +1,32 @@
+// Time  = O(n log m)
+// Space = O(1)
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        for (int i = 0; i < n; i++) {
+
+            int low = 0;
+            int high = m - 1;
+
+            while (low <= high) {
+
+                int mid = low + (high - low) / 2;
+
+                if (matrix[i][mid] == target)
+                    return true;
+
+                if (matrix[i][mid] < target)
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+};
