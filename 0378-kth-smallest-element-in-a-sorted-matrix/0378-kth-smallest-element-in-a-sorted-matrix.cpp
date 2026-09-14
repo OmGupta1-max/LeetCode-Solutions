7e40c4f2386a72e138ff9d2d0688e9aed52c7df6
Om Gupta <guptaom0003@gmail.com>
2026-09-14 22:47:17 +0530

    Time: 16 ms (10.73%), Space: 18.3 MB (42.15%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -4,16 +4,37 @@ public:
 
         int n = matrix.size();
 
-        vector<int> arr;
+        // {value, row, column}
+        priority_queue<
+            tuple<int, int, int>,
+            vector<tuple<int, int, int>>,
+            greater<tuple<int, int, int>>
+        > pq;
 
+        // Put first element of every row
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                arr.push_back(matrix[i][j]);
+            pq.push({matrix[i][0], i, 0});
+        }
+
+        int answer = 0;
+
+        for (int count = 0; count < k; count++) {
+
+            auto [value, row, col] = pq.top();
+            pq.pop();
+
+            answer = value;
+
+            // Add next element from same row
+            if (col + 1 < n) {
+                pq.push({
+                    matrix[row][col + 1],
+                    row,
+                    col + 1
+                });
             }
         }
 
-        sort(arr.begin(), arr.end());
-
-        return arr[k - 1];
+        return answer;
     }
 };
