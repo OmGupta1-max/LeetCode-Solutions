dc782322fdc4b413557111e5b9e473e8793b450b
Om Gupta <guptaom0003@gmail.com>
2026-09-16 19:22:52 +0530

    Add optimal solution for reversing words in a string
    Implement a solution to reverse the words in a string.

---
+++ 0151-reverse-words-in-a-string/optimal 2.0.cpp
@@ -0,0 +1,22 @@
+// Time = O(n)
+// Space = O(n)
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        vector<string> words;
+        string word;
+        while (ss >> word) {
+            words.push_back(word);
+        }
+        string ans;
+        for (int i = words.size() - 1; i >= 0; i--) {
+            if (!ans.empty()) {
+                ans += ' ';
+            }
+            ans += words[i];
+        }
+        return ans;
+    }
+};
